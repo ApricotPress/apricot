0fbc604993048615fb265805194a9602017e48e9
Yegor Zhumikov <yegor@yogurtthehor.se>
2025-07-28 23:46:59 +0200

    rework quit lifecycle and made it more straightforward

--- Source/Apricot/Jar.cs
+++ Source/Apricot/Jar.cs
@@ -53,6 +53,7 @@ public class Jar(
         }
 
         logger.LogInformation("Ending game loop as State is now {State}", State);
+        AfterRun();
     }
 
     public virtual void PrepareToRun()
@@ -61,13 +62,31 @@ public class Jar(
         State = JarState.Running;
     }
 
+    public virtual void AfterRun()
+    {
+        logger.LogInformation("Finalising jar life cycle");
+        services.CastCallback<IJarLifecycleListener>(x => x.OnBeforeQuit());
+
+        logger.LogInformation("Closing all windows");
+        foreach (var window in windows.Windows)
+        {
+            window.Close();
+        }
+
+        logger.LogInformation("Calling quit on all subsystems");
+        foreach (var subsystem in subsystems)
+        {
+            subsystem.Quit();
+        }
+    }
+
     public virtual void Tick()
     {
         foreach (var subsystem in _subsystems)
         {
             subsystem.BeforeFrame();
         }
-        
+
         while (scheduler.HasPending)
         {
             scheduler.DoPending();
@@ -80,23 +99,6 @@ public class Jar(
 
         logger.LogInformation("Quit was requested");
         State = JarState.Exiting;
-
-        // todo: rethink quit lifecycle...
-        services.CastCallback<IJarLifecycleListener>(x => x.OnBeforeQuit());
-
-        scheduler.Schedule(() =>
-            {
-                foreach (var window in windows.Windows)
-                {
-                    window.Close();
-                }
-            }
-        );
-
-        foreach (var subsystem in subsystems)
-        {
-            subsystem.Quit();
-        }
     }
 
     protected virtual void DoInitialization()
@@ -112,6 +114,8 @@ public class Jar(
 
     private void OnMainWindowClosed(IWindow mainWindow)
     {
+        if (State != JarState.Running) return;
+
         mainWindow.OnClose -= OnMainWindowClosed;
         Quit();
     }
