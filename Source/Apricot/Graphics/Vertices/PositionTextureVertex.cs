6b72249953cdccdc7bfc115d662886ee1e4fc462
Yegor Zhumikov <yegor@yogurtthehor.se>
2025-08-14 16:37:27 +0200

    PositionTextureVertex file rename

--- Source/Apricot/Graphics/Vertices/PositionTextureVertex.cs
+++ Source/Apricot/Graphics/Vertices/PositionTextureVertex.cs
@@ -6,7 +6,7 @@ namespace Apricot.Graphics.Vertices;
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
 public struct PositionTextureVertex(Vector3 position, Vector2 uv) : IVertex
 {
-    public static VertexFormat Format { get; } = new VertexFormat([
+    public static VertexFormat Format { get; } = new([
         new VertexFormat.Element(0, VertexElementFormat.Vector3, false),
         new VertexFormat.Element(1, VertexElementFormat.Vector2, false)
     ]);
