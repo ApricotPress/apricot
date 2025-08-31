e02418928b82febc6d3a96a2457b48e4f77fba71
Yegor Zhumikov <yegor@yogurtthehor.se>
2025-08-31 03:00:52 +0200

    optimize logo view game to not upload buffers all the time

--- Source/Apricot.Essentials/Sandbox/LogoView.cs
+++ Source/Apricot.Essentials/Sandbox/LogoView.cs
@@ -17,62 +17,64 @@ namespace Apricot.Essentials.Sandbox;
 /// <summary>
 /// Game that just shows image with logo at center of the screen.
 /// </summary>
-public class LogoView(
-    IGraphics graphics,
-    IWindowsManager windows,
-    IResourcesLoader resources
-) : Game
+public class LogoView : Game
 {
-    private readonly IRenderTarget _mainTarget = graphics.GetWindowRenderTarget(windows.GetOrCreateDefaultWindow());
+    private readonly IGraphics _graphics;
+    private readonly DrawCommand _drawLogoCommand;
 
-    private readonly VertexBuffer<ImGuiVertex> _vertices = graphics
-        .CreateVertexBuffer<ImGuiVertex>(
-            "quad vertices",
-            4
-        );
-
-    private readonly Texture _logo = resources.Load<Texture>(EssentialsIds.Textures.ApricotLogo);
-
-    private readonly IndexBuffer _indices = graphics
-        .CreateIndexBuffer(
-            "indices",
-            IndexSize._4,
-            6
-        );
-
-    private readonly Material _mat = new(
-        resources.Load<ShaderProgram>(EssentialsIds.Shaders.StandardVertex),
-        resources.Load<ShaderProgram>(EssentialsIds.Shaders.StandardFragment)
-    );
-
-    public override void Update() => ClearColor = new PackedColor(255, 217, 132, 255);
-
-
-    public override void Render()
+    public LogoView(
+        IGraphics graphics,
+        IWindowsManager windows,
+        IResourcesLoader resources
+    )
     {
-        var mat =
-            Matrix4x4.CreateScale(300, 300, 1.0f)
-            * Matrix4x4.CreateTranslation(_mainTarget.Width / 2f, _mainTarget.Height / 2f, 0)
-            * Matrix4x4.CreateOrthographicOffCenter(0, _mainTarget.Width, _mainTarget.Height, 0, 0.1f, 1000.0f);
-        _indices.UploadData([0, 1, 2, 0, 2, 3]);
+        _graphics = graphics;
+        var mainTarget = graphics.GetWindowRenderTarget(windows.GetOrCreateDefaultWindow());
 
-        _mat.VertexStage.SetUniformBuffer(mat);
-        _mat.FragmentStage.Samplers[0] = new BoundSampler(
-            _logo,
-            new TextureSampler()
-        );
-        _vertices.UploadData([
+        var vertices = graphics
+            .CreateVertexBuffer<ImGuiVertex>(
+                "quad vertices",
+                4
+            );
+        vertices.UploadData([
             new ImGuiVertex(new Vector2(-1, -1), new Vector2(0, 0), Color.White),
             new ImGuiVertex(new Vector2(-1, 1), new Vector2(0, 1), Color.White),
             new ImGuiVertex(new Vector2(1, 1), new Vector2(1, 1), Color.White),
             new ImGuiVertex(new Vector2(1, -1), new Vector2(1, 0), Color.White),
         ]);
 
-        graphics.Submit(new DrawCommand(_mainTarget, _mat, _vertices)
+        var indices = graphics
+            .CreateIndexBuffer(
+                "indices",
+                IndexSize._4,
+                6
+            );
+        indices.UploadData([0, 1, 2, 0, 2, 3]);
+
+        var mat = new Material(
+            resources.Load<ShaderProgram>(EssentialsIds.Shaders.StandardVertex),
+            resources.Load<ShaderProgram>(EssentialsIds.Shaders.StandardFragment)
+        );
+
+        mat.VertexStage.SetUniformBuffer(
+            Matrix4x4.CreateScale(300, 300, 1.0f)
+            * Matrix4x4.CreateTranslation(mainTarget.Width / 2f, mainTarget.Height / 2f, 0)
+            * Matrix4x4.CreateOrthographicOffCenter(0, mainTarget.Width, mainTarget.Height, 0, 0.1f, 1000.0f)
+        );
+        mat.FragmentStage.Samplers[0] = new BoundSampler(
+            resources.Load<Texture>(EssentialsIds.Textures.ApricotLogo),
+            new TextureSampler()
+        );
+        _drawLogoCommand = new DrawCommand(mainTarget, mat, vertices)
         {
-            IndexBuffer = _indices,
-            IndicesCount = _indices.Capacity,
-            VerticesCount = _vertices.Capacity
-        });
+            IndexBuffer = indices,
+            IndicesCount = indices.Capacity,
+            VerticesCount = vertices.Capacity
+        };
     }
+
+    public override void Update() => ClearColor = new PackedColor(255, 217, 132, 255);
+
+
+    public override void Render() => _graphics.Submit(_drawLogoCommand);
 }
